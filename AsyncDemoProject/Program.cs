e154920770deb6dd46208d55107a8c746c424417
dmytro-buhai <dmytro.buhai@outlook.com>
2025-12-09 22:16:53 +0100

    Add project files.

---
+++ AsyncDemoProject/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace AsyncDemoProject;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var client = new HttpClient();
+        var task = await client.GetStringAsync("https://google.com");
+        int a = 0;
+
+        for (int i = 0; i < 1_000_000; i++)
+        {
+            a = i + 1;
+        }
+
+        var task2 = await client.GetStringAsync("https://google.com");
+        Console.WriteLine("Hello World");
+    }
+}
